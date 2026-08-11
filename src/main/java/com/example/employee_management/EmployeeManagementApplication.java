06bbbd8709af778cb14763d617c9c4f99ba267ea
hiteshburela <ubuntu@ip-172-31-18-246.ap-south-2.compute.internal>
2026-08-11 18:17:51 +0000

    Prepare application for Jenkins Docker deployment

--- src/main/java/com/example/employee_management/EmployeeManagementApplication.java
+++ src/main/java/com/example/employee_management/EmployeeManagementApplication.java
@@ -1,4 +1,4 @@
-package com.example.employee_management;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmployeeManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeeManagementApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementApplication.class, args);
+    }
 }

6b79482b7cf66e1566aa53c5b64be8d5657163c1
hiteshburela <ubuntu@ip-172-31-18-246.ap-south-2.compute.internal>
2026-08-12 06:20:20 +0000

    Fix test after CI/CD failure validation

--- src/test/java/com/example/employee_management/EmployeeManagementApplicationTests.java
+++ src/test/java/com/example/employee_management/EmployeeManagementApplicationTests.java
@@ -3,13 +3,11 @@ package com.example.employee_management;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 @SpringBootTest
 class EmployeeManagementApplicationTests {
 
     @Test
     void contextLoads() {
-        fail("Intentional failure for CI/CD testing");
     }
 }
+

aa87e34c3a496ed0ef28d1a11a590a22b70f821a
hiteshburela <ubuntu@ip-172-31-18-246.ap-south-2.compute.internal>
2026-08-12 06:17:34 +0000

    Test Jenkins failure handling

--- src/test/java/com/example/employee_management/EmployeeManagementApplicationTests.java
+++ src/test/java/com/example/employee_management/EmployeeManagementApplicationTests.java
@@ -3,11 +3,13 @@ package com.example.employee_management;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class EmployeeManagementApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        fail("Intentional failure for CI/CD testing");
+    }
 }
